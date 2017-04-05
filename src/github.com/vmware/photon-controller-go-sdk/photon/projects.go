230b20ffd757ec5a74366c2864bda5d13ff04c2c
Chen Xi <chenxi@vmware.com>
2017-04-05 11:10:13 -0700

    Upgrade Bosh’s Go SDK to Photon Controller 1.2
    - This updates to the Go SDK for Photon Controller 1.2
    - This updates the Bosh Photon CPI to include versioning
    
    Change-Id: I38ffbd6c71eef10f05d9696e96480bd1f956a0b7

--- src/github.com/vmware/photon-controller-go-sdk/photon/projects.go
+++ src/github.com/vmware/photon-controller-go-sdk/photon/projects.go
@@ -12,6 +12,8 @@ package photon
 import (
 	"bytes"
 	"encoding/json"
+	"io"
+	"io/ioutil"
 )
 
 // Contains functionality for projects API.
@@ -29,7 +31,12 @@ type VmGetOptions struct {
 	Name string `urlParam:"name"`
 }
 
-var projectUrl string = "/projects/"
+// Options for GetRouters API.
+type RouterGetOptions struct {
+	Name string `urlParam:"name"`
+}
+
+var projectUrl string = rootUrl + "/projects/"
 
 // Deletes the project with specified ID. Any VMs, disks, etc., owned by the project must be deleted first.
 func (api *ProjectsAPI) Delete(projectID string) (task *Task, err error) {
@@ -131,14 +138,14 @@ func (api *ProjectsAPI) GetVMs(projectID string, options *VmGetOptions) (result 
 	return
 }
 
-// Creates a cluster on the specified project.
-func (api *ProjectsAPI) CreateCluster(projectID string, spec *ClusterCreateSpec) (task *Task, err error) {
+// Creates a service on the specified project.
+func (api *ProjectsAPI) CreateService(projectID string, spec *ServiceCreateSpec) (task *Task, err error) {
 	body, err := json.Marshal(spec)
 	if err != nil {
 		return
 	}
 	res, err := api.client.restClient.Post(
-		api.client.Endpoint+projectUrl+projectID+"/clusters",
+		api.client.Endpoint+projectUrl+projectID+"/services",
 		"application/json",
 		bytes.NewReader(body),
 		api.client.options.TokenOptions)
@@ -150,15 +157,27 @@ func (api *ProjectsAPI) CreateCluster(projectID string, spec *ClusterCreateSpec)
 	return
 }
 
-// Gets clusters for project with the specified ID
-func (api *ProjectsAPI) GetClusters(projectID string) (result *Clusters, err error) {
-	uri := api.client.Endpoint + projectUrl + projectID + "/clusters"
+// Creates an image on the specified project.
+func (api *ProjectsAPI) CreateImage(projectID string, reader io.ReadSeeker, name string, options *ImageCreateOptions) (task *Task, err error) {
+	params := imageCreateOptionsToMap(options)
+	res, err := api.client.restClient.MultipartUpload(api.client.Endpoint+projectUrl+projectID+"/images", reader, name, params, api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	result, err := getTask(getError(res))
+	return result, err
+}
+
+// Gets services for project with the specified ID
+func (api *ProjectsAPI) GetServices(projectID string) (result *Services, err error) {
+	uri := api.client.Endpoint + projectUrl + projectID + "/services"
 	res, err := api.client.restClient.GetList(api.client.Endpoint, uri, api.client.options.TokenOptions)
 	if err != nil {
 		return
 	}
 
-	result = &Clusters{}
+	result = &Services{}
 	err = json.Unmarshal(res, result)
 	return
 }
@@ -186,4 +205,157 @@ func (api *ProjectsAPI) SetSecurityGroups(projectID string, securityGroups *Secu
 
 func (api *ProjectsAPI) getEntityUrl(id string) string {
 	return api.client.Endpoint + projectUrl + id
+}
+
+// Creates a router on the specified project.
+func (api *ProjectsAPI) CreateRouter(projectID string, spec *RouterCreateSpec) (task *Task, err error) {
+	body, err := json.Marshal(spec)
+	if err != nil {
+		return
+	}
+	res, err := api.client.restClient.Post(
+		api.client.Endpoint+projectUrl+projectID+"/routers",
+		"application/json",
+		bytes.NewReader(body),
+		api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	task, err = getTask(getError(res))
+	return
+}
+
+// Gets routers for project with the specified ID, using options to filter the results.
+// If options is nil, no filtering will occur.
+func (api *ProjectsAPI) GetRouters(projectID string, options *RouterGetOptions) (result *Routers, err error) {
+	uri := api.client.Endpoint + projectUrl + projectID + "/routers"
+	if options != nil {
+		uri += getQueryString(options)
+	}
+	res, err := api.client.restClient.GetList(api.client.Endpoint, uri, api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+
+	result = &Routers{}
+	err = json.Unmarshal(res, result)
+	return
+}
+
+// Get quota for project with the specified ID.
+func (api *ProjectsAPI) GetQuota(projectId string) (quota *Quota, err error) {
+	uri := api.client.Endpoint + projectUrl + projectId + "/quota"
+	res, err := api.client.restClient.Get(uri, api.client.options.TokenOptions)
+
+	if err != nil {
+		return
+	}
+
+	defer res.Body.Close()
+	res, err = getError(res)
+	if err != nil {
+		return
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	quota = &Quota{}
+	err = json.Unmarshal(body, quota)
+	return
+}
+
+// Set (replace) the whole project quota with the quota line items specified in quota spec.
+func (api *ProjectsAPI) SetQuota(projectId string, spec *QuotaSpec) (task *Task, err error) {
+	task, err = api.modifyQuota("PUT", projectId, spec)
+	return
+}
+
+// Update portion of the project quota with the quota line items specified in quota spec.
+func (api *ProjectsAPI) UpdateQuota(projectId string, spec *QuotaSpec) (task *Task, err error) {
+	task, err = api.modifyQuota("PATCH", projectId, spec)
+	return
+}
+
+// Exclude project quota line items from the specific quota spec.
+func (api *ProjectsAPI) ExcludeQuota(projectId string, spec *QuotaSpec) (task *Task, err error) {
+	task, err = api.modifyQuota("DELETE", projectId, spec)
+	return
+}
+
+// A private common function for modifying quota for the specified project with the quota line items specified
+// in quota spec.
+func (api *ProjectsAPI) modifyQuota(method string, projectId string, spec *QuotaSpec) (task *Task, err error) {
+	body, err := json.Marshal(spec)
+	if err != nil {
+		return
+	}
+	res, err := api.client.restClient.SendRequestCommon(
+		method,
+		api.client.Endpoint+projectUrl+projectId+"/quota",
+		"application/json",
+		bytes.NewReader(body),
+		api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	task, err = getTask(getError(res))
+	return
+}
+
+// Gets IAM Policy of a project.
+func (api *ProjectsAPI) GetIam(projectId string) (policy *[]PolicyEntry, err error) {
+	res, err := api.client.restClient.Get(
+		api.client.Endpoint+projectUrl+projectId+"/iam",
+		api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	res, err = getError(res)
+	if err != nil {
+		return
+	}
+	var result []PolicyEntry
+	err = json.NewDecoder(res.Body).Decode(&result)
+	return &result, err
+}
+
+// Sets IAM Policy on a project.
+func (api *ProjectsAPI) SetIam(projectId string, policy *[]PolicyEntry) (task *Task, err error) {
+	body, err := json.Marshal(policy)
+	if err != nil {
+		return
+	}
+	res, err := api.client.restClient.Post(
+		api.client.Endpoint+projectUrl+projectId+"/iam",
+		"application/json",
+		bytes.NewReader(body),
+		api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	task, err = getTask(getError(res))
+	return
+}
+
+// Modifies IAM Policy on a project.
+func (api *ProjectsAPI) ModifyIam(projectId string, policyDelta *PolicyDelta) (task *Task, err error) {
+	body, err := json.Marshal(policyDelta)
+	if err != nil {
+		return
+	}
+	res, err := api.client.restClient.Patch(
+		api.client.Endpoint+projectUrl+projectId+"/iam",
+		"application/json",
+		bytes.NewReader(body),
+		api.client.options.TokenOptions)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	task, err = getTask(getError(res))
+	return
 }
